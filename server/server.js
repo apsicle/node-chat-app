794b0a20d86f88f84b0ab3295077846b884ccc05
apsicle <rcyan@email.wm.edu>
2017-09-04 19:12:32 -0400

    added home route

--- server/server.js
+++ server/server.js
@@ -2,15 +2,15 @@ const path = require('path');
 const express = require('express');
 
 const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '../public');
 
 var app = express();
 
-const publicPath = path.join(__dirname + '../public')
-
-console.log(__dirname + '/../public');
-console.log(publicPath);
-
 app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+	res.render('index');
+});
 
 app.listen(port, () => {
 	console.log(`Server is up on port ${port}.`);
